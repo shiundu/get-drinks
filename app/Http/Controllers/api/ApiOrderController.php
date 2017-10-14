8fb86709c0471e49c4b1dea6feb1b1237cef2f36
Ben Shiundu <benshiundu@gmail.com>
2017-10-14 21:53:15 +0300

    order

--- app/Http/Controllers/api/ApiOrderController.php
+++ app/Http/Controllers/api/ApiOrderController.php
@@ -93,16 +93,16 @@ class ApiOrderController extends Controller
 
         
         foreach($orders as $order){
-
             $products = DB::table('order_items')
                         ->join('products', 'products.id', '=', 'order_items.product_id')
-                        ->select('order_items.order_id', 'order_items.customer_id', 'order_items.product_id', 'order_items.quantity', 'products.name', 'products.price', 'products.currency')
+                        ->select('order_items.order_id', 'order_items.customer_id', 'order_items.product_id', 
+                            'order_items.quantity', 'products.name', 'products.price', 'products.currency')
                         ->where('order_id', $order['order_id'])
                         ->get();
 
             // $d = array($order , 'products' => $products);
             // array_push($all_orders, $d);
-            array_push($all_orders, $products);
+            array_push($all_orders, $order['order_id']);
         }
         return $all_orders;
         
