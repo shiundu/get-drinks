5f5813549145d09fc5beafc724bf94f7fc7d4a5e
Ben Shiundu <benshiundu@gmail.com>
2017-10-16 03:05:53 +0300

    order android

--- app/Http/Controllers/api/ApiOrderController.php
+++ app/Http/Controllers/api/ApiOrderController.php
@@ -32,66 +32,66 @@ class ApiOrderController extends Controller
                     ->orWhere('email', $request->customer['email'])
                     ->get();
 
-        // if($customer){
-        //     $order = new Order;
-        //     $order->customer_id = $customer->id;
-        //     $order->user_id = $customer->id;
-        //     $order->products = $products;
-        //     $order->total = $total;
-        //     $order->status = 1;
-        //     $order->drop_off = $request->customer['drop_off'];
+        if($customer){
+            $order = new Order;
+            $order->customer_id = $customer[0]['id'];
+            $order->user_id = $customer[0]['id'];
+            $order->products = $products;
+            $order->total = $total;
+            $order->status = 1;
+            $order->drop_off = $request->customer['drop_off'];
 
-        //     if($order->save()){
-        //         $order_id = $order->id;
-        //         foreach ($request->products as $key => $product) {
+            if($order->save()){
+                $order_id = $order->id;
+                foreach ($request->products as $key => $product) {
 
-        //             foreach ($product as $key => $value) {
-        //                 if($key == 'quantity' && count($value) > 0)
-        //                 {
-        //                     $order_items = new Order_items;
-        //                     $order_items->order_id = $order_id;
-        //                     $order_items->customer_id = $customer->id;
-        //                     $order_items->user_id = $customer->id;
-        //                     $order_items->product_id = $product['product_id'];
-        //                     $order_items->quantity = $product['quantity'];
-        //                     $order_items->save();
-        //                 }
+                    foreach ($product as $key => $value) {
+                        if($key == 'quantity' && count($value) > 0)
+                        {
+                            $order_items = new Order_items;
+                            $order_items->order_id = $order_id;
+                            $order_items->customer_id = $customer[0]['id'];
+                            $order_items->user_id = $customer[0]['id'];
+                            $order_items->product_id = $product['product_id'];
+                            $order_items->quantity = $product['quantity'];
+                            $order_items->save();
+                        }
                     
-        //             }
-        //         }
-        //     }
-        // }
+                    }
+                }
+            }
+        }
 
         
-        // $orders = Order::where('customer_id', $customer->id)
-        //          ->where('status', 1)
-        //          ->get();
-        // $all_orders  = [];
-        // array_push($all_orders, $orders);
+        $orders = Order::where('customer_id', $customer[0]['id'];)
+                 ->where('status', 1)
+                 ->get();
+        $all_orders  = [];
+        array_push($all_orders, $orders);
 
-        // $prod = DB::table('order_items')
-        //         ->join('products', 'products.id', '=', 'order_items.product_id')
-        //         ->select('order_items.order_id', 'order_items.customer_id', 'order_items.product_id', 
-        //             'order_items.quantity', 'products.name', 'products.price', 'products.currency')
-        //         ->where('order_id', $orders[0]['id'])
-        //         ->get();
+        $prod = DB::table('order_items')
+                ->join('products', 'products.id', '=', 'order_items.product_id')
+                ->select('order_items.order_id', 'order_items.customer_id', 'order_items.product_id', 
+                    'order_items.quantity', 'products.name', 'products.price', 'products.currency')
+                ->where('order_id', $orders[0]['id'])
+                ->get();
 
-        // $products = array("products"=> $prod);  
+        $products = array("products"=> $prod);  
    
-        // array_merge($all_orders, []);
-        // array_push($all_orders, $products); 
+        array_merge($all_orders, []);
+        array_push($all_orders, $products); 
         
-        // return $all_orders;
+        return $all_orders;
 
-        return $customer;
+        // return $customer;
         
     }
 
     
     public function pending_orders($phone_number)
     {
-        $customer = Customer::where('phone_number', $phone_number)->first();
-        $orders = Order::where('customer_id', $customer->id)
+        $customer = Customer::where('phone_number', $phone_number)->get();
+        $orders = Order::where('customer_id', $customer[0]['id'])
                  ->where('status', 1)
                  ->get();
         $all_orders  = [];
