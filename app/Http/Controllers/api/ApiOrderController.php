8aec8ac26998b00088723a1e6b5ea4c33fca9f98
Ben Shiundu <benshiundu@gmail.com>
2017-10-16 03:29:55 +0300

    order android

--- app/Http/Controllers/api/ApiOrderController.php
+++ app/Http/Controllers/api/ApiOrderController.php
@@ -91,27 +91,25 @@ class ApiOrderController extends Controller
     public function pending_orders($phone_number)
     {
         $customer = Customer::where('phone_number', $phone_number)->get();
-        // $orders = Order::where('customer_id', $customer[0]['id'])
-        //          ->where('status', 1)
-        //          ->get();
-        // $all_orders  = [];
-        // array_push($all_orders, $orders);
+        $orders = Order::where('customer_id', $customer[0]->id)
+                 ->where('status', 1)
+                 ->get();
+        $all_orders  = [];
+        array_push($all_orders, $orders);
 
-        // $prod = DB::table('order_items')
-        //         ->join('products', 'products.id', '=', 'order_items.product_id')
-        //         ->select('order_items.order_id', 'order_items.customer_id', 'order_items.product_id', 
-        //             'order_items.quantity', 'products.name', 'products.price', 'products.currency')
-        //         ->where('order_id', $orders[0]['id'])
-        //         ->get();
+        $prod = DB::table('order_items')
+                ->join('products', 'products.id', '=', 'order_items.product_id')
+                ->select('order_items.order_id', 'order_items.customer_id', 'order_items.product_id', 
+                    'order_items.quantity', 'products.name', 'products.price', 'products.currency')
+                ->where('order_id', $orders[0]['id'])
+                ->get();
 
-        // $products = array("products"=> $prod);  
+        $products = array("products"=> $prod);  
    
-        // array_merge($all_orders, []);
-        // array_push($all_orders, $products); 
+        array_merge($all_orders, []);
+        array_push($all_orders, $products); 
         
-        // return $all_orders;
-
-        return $customer[0]->id;
+        return $all_orders;
     }
 
 
